2a7752445cb1106220bc4feb02e8c7fcfdc30512
berghach <berghach.chemseddine@gmail.com>
2024-04-05 16:28:52 +0000

    start the migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Relation::enforceMorphMap([
+            'users' => \App\Models\User::class,
+            'companies' => \App\Models\Company::class,
+            'employees' => \App\Models\Employee::class,
+            'freelancers' => \App\Models\Freelancer::class,
+            'operators' => \App\Models\Operator::class,
+            'admins' => \App\Models\Admin::class,
+        ]);
     }
 }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,14 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->nullable();
+            // $table->enum('role', ['admin', 'user', 'employee', 'freelancer', 'company', 'operator'])->default('user');
+            $table->string('industry')->nullable();
+            $table->string('bio')->nullable();
+            $table->string('location')->nullable();
+            $table->string('job')->nullable();
+            $table->string('company_name')->nullable();
+            $table->string('company_headquarter')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
