9818b3146f563eb545b7229beecca031224920a2
berghach <berghach.chemseddine@gmail.com>
2024-04-18 17:22:01 +0000

    start the auth

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,19 +4,20 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ config('app.name') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
         <!-- Styles -->
-        
+        {{-- <link rel="stylesheet" href={{ asset('css/input.css') }}> --}}
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         @foreach ($collection = App\Enums\Contract::getValues() as $item)
             <div>{{$item}}</div>
         @endforeach
-        <div>{{App\Enums\Contract::Internship()}}</div>
+        <div class=" bg-amber-400">{{App\Enums\Contract::Internship()}}</div>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    if(Auth::check()){
+        return view('homepage');
+    }else{
+        return redirect()->route('login');
+    }
 });
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
