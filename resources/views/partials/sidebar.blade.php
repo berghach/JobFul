08c052cfdf27778a434011576ccda8220c8e9cd0
berghach <berghach.chemseddine@gmail.com>
2024-05-03 17:23:23 +0000

    commit

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -37,25 +37,31 @@
         </ul>
     </div>
 </aside>
-<div class="mt-32 lg:hidden">
+<div class="mt-32 lg:hidden ps-3">
     <button id="sidenav-button" class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center rounded-full bg-primary" type="button">
-        {{-- @switch(request()->rou)
-            @case(1)
-                
+        @switch(Route::currentRouteName())
+            @case('admin-dashboard')
+                <i data-feather="activity"></i>Dashboard
                 @break
-            @case(2)
-                
+            @case('operator-dashboard')
+                <i data-feather="activity"></i>Dashboard
+                @break
+            @case('users.index')
+                <i data-feather="users"></i>Users
+                @break
+            @case('companies.index')
+                <i data-feather="briefcase"></i>Companies
                 @break
             @default
                 
-        @endswitch --}}
-        {{request()->routeName()}}
+        @endswitch
+        {{-- {{Route::currentRouteName()}} --}}
         <i data-feather="chevron-down"></i>
     </button>
     <div id="sidenav-states" class="z-10  bg-white divide-y divide-gray-100 rounded-lg shadow w-44 ">
         <ul class="py-2 text-sm text-gray-700 gap-y-2">
             <li>
-                <a href="" class="inline-flex w-full px-4 py-2 text-sm text-gray-700 hover:bg-primary ease-in-out duration-200 {{request()->routeIs('admin-dashboard')||request()->routeIs('operator-dashboard') ? 'bg-primary' : ''}}">
+                <a href={{ Auth::user()->role->name === 'admin' ? route('admin-dashboard') : route('operator-dashboard') }} class="inline-flex w-full px-4 py-2 text-sm text-gray-700 hover:bg-primary ease-in-out duration-200 {{request()->routeIs('admin-dashboard')||request()->routeIs('operator-dashboard') ? 'bg-primary' : ''}}">
                     <div class="inline-flex items-center gap-2">
                         <i data-feather="activity"></i>Dashboard
                     </div>
@@ -63,14 +69,14 @@
             </li>
             @if (Auth::user()->role->name === 'admin')
                 <li>
-                    <a href="" class="inline-flex w-full px-4 py-2 text-sm text-gray-700 hover:bg-primary ease-in-out duration-200 {{request()->routeIs('users.index') ? 'bg-primary' : ''}}">
+                    <a href="{{ route('users.index') }}" class="inline-flex w-full px-4 py-2 text-sm text-gray-700 hover:bg-primary ease-in-out duration-200 {{request()->routeIs('users.index') ? 'bg-primary' : ''}}">
                         <div class="inline-flex items-center gap-2">
                             <i data-feather="users"></i>Users
                         </div>
                     </a>
                 </li>
                 <li>
-                    <a href="" class="inline-flex w-full px-4 py-2 text-sm text-gray-700 hover:bg-primary ease-in-out duration-200 {{request()->routeIs('companies.index') ? 'bg-primary' : ''}}">
+                    <a href="{{ route('companies.index') }}" class="inline-flex w-full px-4 py-2 text-sm text-gray-700 hover:bg-primary ease-in-out duration-200 {{request()->routeIs('companies.index') ? 'bg-primary' : ''}}">
                         <div class="inline-flex items-center gap-2">
                             <i data-feather="briefcase"></i>Companies
                         </div>
@@ -94,6 +100,21 @@
                     <a href="" class="inline-flex w-full px-4 py-2 text-sm text-gray-700 hover:bg-primary ease-in-out duration-200 ">
                         <div class="inline-flex items-center gap-2">
                             <i data-feather="tag"></i>Tags
+                        </div>
+                    </a>
+                </li>
+            @else
+                <li>
+                    <a href="" class="inline-flex w-full px-4 py-2 text-sm text-gray-700 hover:bg-primary ease-in-out duration-200 ">
+                        <div class="inline-flex items-center gap-2">
+                            <i data-feather="folder"></i>Applications
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="" class="inline-flex w-full px-4 py-2 text-sm text-gray-700 hover:bg-primary ease-in-out duration-200 ">
+                        <div class="inline-flex items-center gap-2">
+                            <i data-feather="book-open"></i>Posts
                         </div>
                     </a>
                 </li>
